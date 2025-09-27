f569e4410b786714a5fc446cebbc1d4760fcd7d7
Eriton Longui <7eriton@gmail.com>
2025-09-26 22:06:39 -0300

    Adicionando favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,13 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap"
         />
 
+        {/* Favicon */}
+        <link
+          rel="icon"
+          href="/favicon.svg"
+          type="image/svg"
+        />
+
         {/* Phosphor Icons */}
         <link
           rel="stylesheet"
